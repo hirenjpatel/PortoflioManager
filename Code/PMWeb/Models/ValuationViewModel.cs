2d48e9bffea90638c172b4801cb25ebdfffb3fc9
Hiren Patel <hiren85@gmail.com>
2017-03-01 19:00:56 -0600

    Latest Changes

---
+++ Code/PMWeb/Models/ValuationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNet.Highcharts;
+using DotNet.Highcharts.Enums;
+using DotNet.Highcharts.Helpers;
+using DotNet.Highcharts.Options;
+
+namespace PMWeb.Models
+{
+    public class ValuationViewModel
+    {
+        public PMApi.Models.ValuationSummary ValuationSummary { get; set; }
+
+        public Highcharts Chart { get; set; }
+
+        public string PercentageChange { get; set; }
+
+        public string LastChange { get; set; }
+
+        public string PortfolioValue { get; set; }
+
+        public string IntradayChange { get; set; }
+        public string IntradayLow { get; set; }
+        public string IntradayHigh { get; set; }
+    }
+}
